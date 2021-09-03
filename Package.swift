3aa542c8eaf73492e4cb295478582cb06189fbab
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-09-03 18:25:20 +0300

    no message

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Constraints",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v9), .macOS(.v10_14), .tvOS(.v11), .watchOS(.v3)],
+    products: [.library(name: "Constraints", targets: ["Constraints"])],
+    dependencies: [],
+    targets: [.target(name: "Constraints", dependencies: []),]
+)
