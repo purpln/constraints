3381c053317c2ba1fc06f32f5746b38f1bf42e73
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-09-10 17:09:45 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Constraints",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v9), .macOS(.v10_14), .tvOS(.v11), .watchOS(.v3)],
+    platforms: [.iOS(.v10)],
     products: [.library(name: "Constraints", targets: ["Constraints"])],
     dependencies: [],
     targets: [.target(name: "Constraints")]
